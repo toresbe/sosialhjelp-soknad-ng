18aa4a4edde6dd63954546e924532ea593a3507c
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-09-23 13:44:20 +0200

    Redirect to new application after creation

--- src/components/NySoknad.tsx
+++ src/components/NySoknad.tsx
@@ -1,15 +1,19 @@
 import {useMutation} from "@apollo/client";
 import {NySoknadDocument, NySoknadMutation} from "../generated/apollo";
+import {useRouter} from "next/router";
 
 // Just a placeholder page for now.
 export const NySoknad = () => {
     const [mutation] = useMutation<NySoknadMutation>(NySoknadDocument);
+    const router = useRouter();
 
     return (
         <div>
             <button
                 onClick={async () => {
-                    console.log(await mutation());
+                    const stuff = await mutation();
+                    const nySoknadId = stuff.data?.nySoknad.id;
+                    router.push(`${nySoknadId}/1`);
                 }}
             >
                 Opprett søknad
