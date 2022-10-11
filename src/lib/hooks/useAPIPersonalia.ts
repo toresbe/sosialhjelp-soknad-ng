8a5ea178cf9dfb5cf2a0687b2099bebc0dd960ed
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-10-11 11:18:47 +0200

    ignore eslint cache

--- src/lib/hooks/useAPIPersonalia.ts
+++ src/lib/hooks/useAPIPersonalia.ts
@@ -1,14 +1,10 @@
 import {useQuery} from "@apollo/client";
 import {GetPersonaliaDocument, GetPersonaliaQuery} from "../../generated/apolloClientTypes";
-import {useSoknadIdFromRouter} from "../soknadContext/useSoknadIdFromRouter";
 import {BasisPersonaliaProps} from "../../components/personalia/BasisPersonalia";
 
-export const usePersonalia = (): BasisPersonaliaProps => {
-    const soknadId = useSoknadIdFromRouter();
-
+export const useGetPersonaliaQuery = (soknadId: string): BasisPersonaliaProps => {
     const {data, error} = useQuery<GetPersonaliaQuery>(GetPersonaliaDocument, {variables: {soknadId}});
 
-    console.log({data, error});
     return {
         personalia: data?.soknad?.personalia,
     };
